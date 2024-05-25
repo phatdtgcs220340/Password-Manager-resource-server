6a42015157bb06d25e23285bcba31399f7d8b888
phatdtgcs220340@fpt.edu.vn <phatdtgcs220340@fpt.edu.vn>
2024-05-25 15:18:00 +0700

    spring init

---
+++ src/main/java/com/phatdo/resource_server/Configuration/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.phatdo.resource_server.Configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Value("${spring.security.oauth2.resource-server.jwt.jwt-set-uri}")
+    private String keySetUri;
+
+    @Bean
+    public SecurityFilterChain defaultFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(c -> c
+                .anyRequest().authenticated())
+                .oauth2ResourceServer(c -> c
+                        .jwt(j -> j
+                                .jwkSetUri(keySetUri)));
+        return http.build();
+    }
+}

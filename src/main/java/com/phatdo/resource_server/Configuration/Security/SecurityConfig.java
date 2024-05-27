04bc64854b63bc5da262dfa58c923d0fbb231a6a
Đỗ Tấn Phát <114555261+phatdtgcs220340@users.noreply.github.com>
2024-05-27 16:07:28 +0700

    move

--- src/main/java/com/phatdo/resource_server/Configuration/Security/SecurityConfig.java
+++ src/main/java/com/phatdo/resource_server/Configuration/Security/SecurityConfig.java
@@ -1,16 +1,19 @@
-package com.phatdo.resource_server.Configuration;
+package com.phatdo.resource_server.Configuration.Security;
 
+import com.phatdo.resource_server.Configuration.OAuth2.OAuth2SuccessHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
     @Value("${spring.security.oauth2.resource-server.jwt.jwt-set-uri}")
     private String keySetUri;
-
     @Bean
     public SecurityFilterChain defaultFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(c -> c

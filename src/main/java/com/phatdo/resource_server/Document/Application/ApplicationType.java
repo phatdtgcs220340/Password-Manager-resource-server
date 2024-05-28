3598759cdc54cbb57f086814fa23bfc3956b2601
Đỗ Tấn Phát <114555261+phatdtgcs220340@users.noreply.github.com>
2024-05-28 19:44:52 +0700

    add some type

--- src/main/java/com/phatdo/resource_server/Document/Application/ApplicationType.java
+++ src/main/java/com/phatdo/resource_server/Document/Application/ApplicationType.java
@@ -1,5 +1,10 @@
 package com.phatdo.resource_server.Document.Application;
 
 public enum ApplicationType {
-    SOCIAL_MEDIA, WEB_TOOL, BANK_ACCOUNT, CARD, OTHERS
+    SOCIAL_MEDIA,
+    WEB_TOOL,
+    BANK_APPLICATION,
+    SHOPPING_APPLICATION,
+    CARD,
+    OTHERS
 }

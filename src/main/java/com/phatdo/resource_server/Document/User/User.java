ed97ef7fb7ebf228fb0c35f06bca86b4dc1753ae
Đỗ Tấn Phát <114555261+phatdtgcs220340@users.noreply.github.com>
2024-05-28 19:34:25 +0700

    user equal function

--- src/main/java/com/phatdo/resource_server/Document/User/User.java
+++ src/main/java/com/phatdo/resource_server/Document/User/User.java
@@ -23,4 +23,11 @@ public class User {
     public String toString() {
         return String.format("User: %s-%s", username, fullName);
     }
+
+    public boolean equals(User user) {
+        return this.id.equals(user.getId()) &&
+                this.username.equals(user.getUsername()) &&
+                this.fullName.equals(user.getFullName()) &&
+                this.createdAt.equals(user.getCreatedAt());
+    }
 }

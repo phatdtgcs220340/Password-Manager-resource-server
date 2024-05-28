7b1931d9b739be8e1861ea4203cd54608071f348
Đỗ Tấn Phát <114555261+phatdtgcs220340@users.noreply.github.com>
2024-05-28 13:44:43 +0700

    application rest controller

---
+++ src/main/java/com/phatdo/resource_server/Controller/Rest/ApplicationController.java
@@ -0,0 +1,30 @@
+package com.phatdo.resource_server.Controller.Rest;
+
+import com.phatdo.resource_server.CustomContext.UserContext.UserContext;
+import com.phatdo.resource_server.Document.Account.AccountService;
+import com.phatdo.resource_server.Document.Application.Application;
+import com.phatdo.resource_server.Document.User.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/application")
+public class ApplicationController {
+    private final AccountService accountService;
+    @Autowired
+    public ApplicationController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+    @GetMapping()
+    public ResponseEntity<List<Application>> getUserApplications(){
+        User user = UserContext.getUser();
+        List<Application> applications =
+                    accountService.getApplications(user);
+        return ResponseEntity.ok(applications);
+    }
+}

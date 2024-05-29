f72137b87b739c5f9a471243a357f1a1cc55f5d0
Đỗ Tấn Phát <114555261+phatdtgcs220340@users.noreply.github.com>
2024-05-29 22:20:08 +0700

    add cors

--- src/main/java/com/phatdo/resource_server/Configuration/Security/SecurityConfig.java
+++ src/main/java/com/phatdo/resource_server/Configuration/Security/SecurityConfig.java
@@ -7,19 +7,36 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
     @Value("${spring.security.oauth2.resource-server.jwt.jwt-set-uri}")
     private String keySetUri;
+
     @Bean
     public SecurityFilterChain defaultFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(c -> c
-                    .anyRequest().authenticated())
+                        .anyRequest().authenticated())
                 .oauth2ResourceServer(c -> c
                         .jwt(j -> j
                                 .jwkSetUri(keySetUri)));
         return http.build();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://localhost:5173");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
